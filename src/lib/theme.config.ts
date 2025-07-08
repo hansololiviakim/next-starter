69f6aadd16207ccf8b7b32ab8046b33f0e64d250
Hansol Olivia Kim <dev.hansolkim@gmail.com>
2025-07-08 09:12:51 +0900

    chore: vapor 설정

---
+++ src/lib/theme.config.ts
@@ -0,0 +1,8 @@
+import { createThemeConfig } from '@vapor-ui/core'
+
+export const themeConfig = createThemeConfig({
+  appearance: 'light', // 'light', 'dark'
+  radius: 'full',
+  scaling: 1.0,
+  storageKey: 'my-next-app-theme',
+})
